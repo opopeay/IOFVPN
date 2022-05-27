539ee79a67cce34c06ccfbb25c88c4a343243102
iof <76963329+opopeay@users.noreply.github.com>
2022-05-27 14:45:04 +0700

    Add files via upload

---
+++ iof99.php
@@ -0,0 +1,35 @@
+2.7
+อัพเดตวันที่ 27/05/2022
+
+✔️ เพิ่มไฟล์ Server VIP_GAMING
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✔️ เพิ่มเซิฟ SG_Digital,HK,IN,JP
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✔️ แก้ไขเซิฟ VIP SG
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+
+🔖▪️🇹🇭 Free VIP_GAMING
+🔖▪️🇹🇭 Free 3BB Fiber
+🔖▪️🇹🇭 Free True Fiber 
+🔖▪️🇹🇭 Free ViP_CAT 3 Server
+🔖▪️🇹🇭 Free TH-GAME
+🔖▪️🇹🇭 Free TCC Technology 2 Server
+🔖▪️🇸🇬 VIP SG 20 Server
+🔖▪️🇸🇬 SG_Digital 3 Server
+🔖▪️🇭🇰 HONGKONG
+🔖▪️🇮🇳 INDIA
+🔖▪️🇯🇵 JAPAN
+
+🛒Super_ViP สำหรับเช่า 50 บ./เดือน💲
+
+🏷️เล่นเกมส์ให้เลือก VIP_GAMING,TH-GAME
+🏷️ดูยูทูปให้ให้เลือก VIP-SG,3BBFiber,TrueFiber
+🏷️ใช้งานทั่วไปให้เลือก CAT-IDC,TCC
+_____________________________________
+✅ควรเลือกเซิฟให้ตรงกับการใช้งาน
+🔔Reboot Server 03:30 น.
+👤Support AIS,DTAC,TRUE
+
+⛔แจ้งปัญหากดที่ปุ่ม ℹ️ มุมบนขวามือแอพ
+
+🆙การอัพเดต # ควรมีอินเตอร์เน็ตในการอัพเดต
